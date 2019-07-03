5efa18fec6b223ae788a1e6fee4f2497af3b1bec
Altughan Ozengi <31768522+Blockeer09@users.noreply.github.com>
2019-07-03 22:00:32 +0200

    ..

--- js/app.js
+++ js/app.js
@@ -4,13 +4,13 @@ import {about} from './about';
 import {contact} from './contact';
 
 const routes = {
-  "/": home,
-  "/about": about,
-  "/contact": contact
+  "/VanillaJS-SPA-App/": home,
+  "/VanillaJS-SPA-App/about": about,
+  "/VanillaJS-SPA-App/contact": contact
 };
 
 let content = document.getElementById('root');
-let request = window.location.pathname || "/";
+let request = window.location.pathname || "VanillaJS-SPA-App/";
 content.innerHTML = routes[request] ? routes[request] : Error404;
 
 window.onpopstate = () => {
